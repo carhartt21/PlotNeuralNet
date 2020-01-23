0434cb66541c5be2e9ff35c92a4186c7cb348cf4
cg_fgvwds <none>
2020-01-23 10:23:09 +0100

    Major Update

--- pycore/tikzeng.py
+++ pycore/tikzeng.py
@@ -1,14 +1,14 @@
 
 import os
 
-def to_head( projectpath ):
-    pathlayers = os.path.join( projectpath, 'layers/' ).replace('\\', '/')
+def to_head(projectpath):
+    pathlayers = os.path.join(projectpath, 'layers/').replace('\\', '/')
     return r"""
-\documentclass[border=8pt, multi, tikz]{standalone} 
+\documentclass[border=8pt, multi, tikz]{standalone}
 \usepackage{import}
 \subimport{"""+ pathlayers + r"""}{init}
 \usetikzlibrary{positioning}
-\usetikzlibrary{3d} %for including external image 
+\usetikzlibrary{3d} %for including external image
 """
 
 def to_cor():
@@ -19,7 +19,8 @@ def to_cor():
 \def\UnpoolColor{rgb:blue,2;green,1;black,0.3}
 \def\FcColor{rgb:blue,5;red,2.5;white,5}
 \def\FcReluColor{rgb:blue,5;red,5;white,4}
-\def\SoftmaxColor{rgb:magenta,5;black,7}   
+\def\SoftmaxColor{rgb:magenta,5;black,7}
+\def\SumColor{rgb:green, 1}
 """
 
 def to_begin():
@@ -34,20 +35,20 @@ def to_begin():
 
 # layers definition
 
-def to_input( pathfile, to='(-3,0,0)', width=8, height=8, name="temp" ):
+def to_input(pathfile, to='(-3,0,0)', width=8, height=8, name="temp"):
     return r"""
 \node[canvas is zy plane at x=0] (""" + name + """) at """+ to +""" {\includegraphics[width="""+ str(width)+"cm"+""",height="""+ str(height)+"cm"+"""]{"""+ pathfile +"""}};
 """
 
 # Conv
-def to_Conv( name, s_filer=256, n_filer=64, offset="(0,0,0)", to="(0,0,0)", width=1, height=40, depth=40, caption=" " ):
+def to_Conv(name, s_filter=256, n_filter=64, offset="(0,0,0)", to="(0,0,0)", width=1, height=40, depth=40, caption=" "):
     return r"""
-\pic[shift={"""+ offset +"""}] at """+ to +""" 
+\pic[shift={"""+ offset +"""}] at """+ to +"""
     {Box={
         name=""" + name +""",
         caption="""+ caption +r""",
-        xlabel={{"""+ str(n_filer) +""", }},
-        zlabel="""+ str(s_filer) +""",
+        xlabel={{"""+ str(n_filter) +""", }},
+        zlabel="""+ str(s_filter) +""",
         fill=\ConvColor,
         height="""+ str(height) +""",
         width="""+ str(width) +""",
@@ -58,14 +59,14 @@ def to_Conv( name, s_filer=256, n_filer=64, offset="(0,0,0)", to="(0,0,0)", widt
 
 # Conv,Conv,relu
 # Bottleneck
-def to_ConvConvRelu( name, s_filer=256, n_filer=(64,64), offset="(0,0,0)", to="(0,0,0)", width=(2,2), height=40, depth=40, caption=" " ):
+def to_ConvConvRelu(name, s_filter=256, n_filter=(64,64), offset="(0,0,0)", to="(0,0,0)", width=(2,2), height=40, depth=40, caption=" "):
     return r"""
-\pic[shift={ """+ offset +""" }] at """+ to +""" 
+\pic[shift={ """+ offset +""" }] at """+ to +"""
     {RightBandedBox={
         name="""+ name +""",
         caption="""+ caption +""",
-        xlabel={{ """+ str(n_filer[0]) +""", """+ str(n_filer[1]) +""" }},
-        zlabel="""+ str(s_filer) +""",
+        xlabel={{ """+ str(n_filter[0]) +""", """+ str(n_filter[1]) +""" }},
+        zlabel="""+ str(s_filter) +""",
         fill=\ConvColor,
         bandfill=\ConvReluColor,
         height="""+ str(height) +""",
@@ -80,7 +81,7 @@ def to_ConvConvRelu( name, s_filer=256, n_filer=(64,64), offset="(0,0,0)", to="(
 # Pool
 def to_Pool(name, offset="(0,0,0)", to="(0,0,0)", width=1, height=32, depth=32, opacity=0.5, caption=" "):
     return r"""
-\pic[shift={ """+ offset +""" }] at """+ to +""" 
+\pic[shift={ """+ offset +""" }] at """+ to +"""
     {Box={
         name="""+name+""",
         caption="""+ caption +r""",
@@ -93,10 +94,10 @@ def to_Pool(name, offset="(0,0,0)", to="(0,0,0)", width=1, height=32, depth=32, 
     };
 """
 
-# unpool4, 
+# unpool4,
 def to_UnPool(name, offset="(0,0,0)", to="(0,0,0)", width=1, height=32, depth=32, opacity=0.5, caption=" "):
     return r"""
-\pic[shift={ """+ offset +""" }] at """+ to +""" 
+\pic[shift={ """+ offset +""" }] at """+ to +"""
     {Box={
         name="""+ name +r""",
         caption="""+ caption +r""",
@@ -111,14 +112,14 @@ def to_UnPool(name, offset="(0,0,0)", to="(0,0,0)", width=1, height=32, depth=32
 
 
 
-def to_ConvRes( name, s_filer=256, n_filer=64, offset="(0,0,0)", to="(0,0,0)", width=6, height=40, depth=40, opacity=0.2, caption=" " ):
+def to_ConvRes(name, s_filter=256, n_filter=64, offset="(0,0,0)", to="(0,0,0)", width=6, height=40, depth=40, opacity=0.2, caption=" "):
     return r"""
-\pic[shift={ """+ offset +""" }] at """+ to +""" 
+\pic[shift={ """+ offset +""" }] at """+ to +"""
     {RightBandedBox={
         name="""+ name + """,
         caption="""+ caption + """,
-        xlabel={{ """+ str(n_filer) + """, }},
-        zlabel="""+ str(s_filer) +r""",
+        xlabel={{ """+ str(n_filter) + """, }},
+        zlabel="""+ str(s_filter) +r""",
         fill={rgb:white,1;black,3},
         bandfill={rgb:white,1;black,2},
         opacity="""+ str(opacity) +""",
@@ -131,13 +132,13 @@ def to_ConvRes( name, s_filer=256, n_filer=64, offset="(0,0,0)", to="(0,0,0)", w
 
 
 # ConvSoftMax
-def to_ConvSoftMax( name, s_filer=40, offset="(0,0,0)", to="(0,0,0)", width=1, height=40, depth=40, caption=" " ):
+def to_ConvSoftMax(name, s_filter=40, offset="(0,0,0)", to="(0,0,0)", width=1, height=40, depth=40, caption=" "):
     return r"""
-\pic[shift={"""+ offset +"""}] at """+ to +""" 
+\pic[shift={"""+ offset +"""}] at """+ to +"""
     {Box={
         name=""" + name +""",
         caption="""+ caption +""",
-        zlabel="""+ str(s_filer) +""",
+        zlabel="""+ str(s_filter) +""",
         fill=\SoftmaxColor,
         height="""+ str(height) +""",
         width="""+ str(width) +""",
@@ -147,14 +148,14 @@ def to_ConvSoftMax( name, s_filer=40, offset="(0,0,0)", to="(0,0,0)", width=1, h
 """
 
 # SoftMax
-def to_SoftMax( name, s_filer=10, offset="(0,0,0)", to="(0,0,0)", width=1.5, height=3, depth=25, opacity=0.8, caption=" " ):
+def to_SoftMax(name, s_filter=10, offset="(0,0,0)", to="(0,0,0)", width=1.5, height=3, depth=25, opacity=0.8, caption=" "):
     return r"""
-\pic[shift={"""+ offset +"""}] at """+ to +""" 
+\pic[shift={"""+ offset +"""}] at """+ to +"""
     {Box={
         name=""" + name +""",
         caption="""+ caption +""",
         xlabel={{" ","dummy"}},
-        zlabel="""+ str(s_filer) +""",
+        zlabel="""+ str(s_filter) +""",
         fill=\SoftmaxColor,
         opacity="""+ str(opacity) +""",
         height="""+ str(height) +""",
@@ -165,16 +166,28 @@ def to_SoftMax( name, s_filer=10, offset="(0,0,0)", to="(0,0,0)", width=1.5, hei
 """
 
 
-def to_connection( of, to):
+def to_short_connection(of, to):
     return r"""
-\draw [connection]  ("""+of+"""-east)    -- node {\midarrow} ("""+to+"""-west);
+\draw [connection]  ("""+of+"""-east) -- node {\midarrow} ("""+to+"""-west);
 """
 
-def to_skip( of, to, pos=1.25):
+
+def to_long_connection(of, to):
+    return r"""
+\path ("""+ of +"""-southeast) -- ("""+ of +"""-northeast) coordinate[pos=1.25] ("""+ of +"""-top) ;
+\path ("""+ of +"""-top |- """+ to +"""-north) coordinate ("""+ to +"""-top)  ;
+
+\draw [connection]  ("""+ of +"""-northeast)
+-- node {\midline}("""+ of +"""-northeast |- """+ of +"""-top)
+-- node {\midarrow}("""+ of +"""-top -| """+ to +"""-north)
+-- node {\midline}("""+ to +"""-north);
+"""
+
+def to_skip(of, to, pos=1.25):
     return r"""
 \path ("""+ of +"""-southeast) -- ("""+ of +"""-northeast) coordinate[pos="""+ str(pos) +"""] ("""+ of +"""-top) ;
 \path ("""+ to +"""-south)  -- ("""+ to +"""-north)  coordinate[pos="""+ str(pos) +"""] ("""+ to +"""-top) ;
-\draw [copyconnection]  ("""+of+"""-northeast)  
+\draw [copyconnection]  ("""+of+"""-northeast)
 -- node {\copymidarrow}("""+of+"""-top)
 -- node {\copymidarrow}("""+to+"""-top)
 -- node {\copymidarrow} ("""+to+"""-north);
@@ -186,12 +199,23 @@ def to_end():
 \end{document}
 """
 
+def to_add(name, to, offset="(0,0,0)", opacity=0.4, caption=''):
+    return r"""
+\pic[shift={"""+ offset +"""}] at ("""+ to +"""-east)
+    {Ball={
+        name=""" + name +""",
+        caption="""+ caption +""",
+        fill=\SumColor,
+        opacity="""+ str(opacity) +""",
+        radius=2.5,
+        logo=$+$
+        }
+    };
+"""
 
-def to_generate( arch, pathname="file.tex" ):
-    with open(pathname, "w") as f: 
+
+def to_generate(arch, pathname="file.tex"):
+    with open(pathname, "w") as f:
         for c in arch:
             print(c)
-            f.write( c )
-     
-
-
+            f.write(str(c))
